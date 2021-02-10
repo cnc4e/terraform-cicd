8d605a295f60c98a30eaea95cfff2be161619139
narita <narita.ryo@tis.co.jp>
2021-02-10 15:12:51 +0900

    #2 不要ロールを削除

--- terraform/modules/environment/github-runner-ec2/iam.tf
+++ terraform/modules/environment/github-runner-ec2/iam.tf
@@ -25,44 +25,9 @@ data "aws_iam_policy" "systems_manager" {
   arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-data "aws_iam_policy" "access_ecr" {
-  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
-}
-
-resource "aws_iam_policy" "s3objectput" {
-  name        = "${var.pj}-s3objectput"
-  path        = "/"
-  description = "s3にオブジェクトをPUTする"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:PutObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_iam_role_policy_attachment" "systems_manager" {
   role       = aws_iam_role.role.name
   policy_arn = data.aws_iam_policy.systems_manager.arn
-}
-
-resource "aws_iam_role_policy_attachment" "access_ecr" {
-  role       = aws_iam_role.role.name
-  policy_arn = data.aws_iam_policy.access_ecr.arn
-}
-
-resource "aws_iam_role_policy_attachment" "s3objectput" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.s3objectput.arn
 }
 
 resource "aws_iam_instance_profile" "github_runner" {
